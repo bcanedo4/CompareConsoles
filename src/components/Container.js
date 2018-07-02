d097ab952bee4ce6e3bcad414ed6d770d4f8b63e
Brian Canedo <bcanedo4@gmail.com>
2018-07-02 10:59:17 -0700

    Implemented Grid and positioned two main elements

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,12 +2,10 @@ import React, { Component } from 'react';
 import Playstation from './Playstation';
 import Xbox from './Xbox';
 
-
 class Container extends Component {
     render() {
         return (
             <div class="container">
-                The Container
                 <Playstation />
                 <Xbox />
             </div>
