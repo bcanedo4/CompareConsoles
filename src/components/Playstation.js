a07ec75d50fdbba127273ec00b5b55fc3cee7d1f
Brian Canedo <bcanedo4@gmail.com>
2018-07-02 22:30:48 -0700

    Removed Disadvantages Section

--- src/components/Playstation.js
+++ src/components/Playstation.js
@@ -6,8 +6,13 @@ class Playstation extends Component {
             <div class="container__playstation">
                 <div class="play__img"></div>
                 <div class="play__title">PlayStation</div>
-                <div class="play__adv"></div>
-                <div class="play__dis"></div>
+                <ul class="play__adv">
+                    Advantages
+                    <li class="play__adv--item">PS4 Pro costs less</li>
+                    <li class="play__adv--item">Myriad of PS4 exclusive games</li>
+                    <li class="play__adv--item">More PS4 units sold</li>
+                    <li class="play__adv--item">Controller is more comfortable</li>
+                </ul>
             </div>
         );
     }   
--- src/components/Xbox.js
+++ src/components/Xbox.js
@@ -5,6 +5,14 @@ class Xbox extends Component {
         return (
             <div class="container__xbox">
                 <div class="xbox__img"></div>
+                <div class="xbox__title">Xbox</div>
+                <ul class="xbox__adv">
+                    Advantages
+                    <li class="xbox__adv--item">Xbox One X has a faster processor</li>
+                    <li class="xbox__adv--item">Xbox One X can pla yUltra HD Blu-ray discs</li>
+                    <li class="xbox__adv--item">Supports cross platform gaming</li>
+                    <li class="xbox__adv--item">Halo and Gears of War I guess?</li>
+                </ul>
             </div>
         );
     }   
